8e95942184ce1d824884ea8b22e28005780e3612
Nat Greenwald <nat.greenwald@gmail.com>
2016-12-04 16:49:58 -0500

    results port

--- fin_data.py
+++ fin_data.py
@@ -40,7 +40,7 @@ def result():
 
 if __name__ == "__main__":
     # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
     port = int(os.environ.get("PORT", 33507))
+    # app.run(host='0.0.0.0', port=port)
     app.run(debug=True, port=33507)
     # app.run(host='0.0.0.0', debug=True, port=5000)
