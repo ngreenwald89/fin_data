8c5f67336ddf8609412d69eae87514aab1acb8c6
Nat Greenwald <nat.greenwald@gmail.com>
2016-12-04 16:12:40 -0500

    port and app config

--- fin_data.py
+++ fin_data.py
@@ -41,4 +41,6 @@ def result():
 if __name__ == "__main__":
     # port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(debug=True, port=33507)
+    # app.run(host='0.0.0.0', debug=True, port=5000)
